e4ea1fa282a941465eeb38024a0fdd004d1ff9e7
Brian <chumobrian@gmail.com>
2023-12-19 14:27:06 +0300

    feat(database): Add a query that  prepares a MySQL server environment for development

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,11 @@
 -- creates a database hbnb_test_db
-CREATE DATABASE IF NOT EXISTS hbtn_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- creates a new user hbnb_test (in localhost) with hbnb_test_pwd password
-CREATE USER IF NOT EXISTS hbtn_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 
--- Grant privileges all privileges on hbtn_test_db to hbtn_test
-GRANT ALL PRIVILEGES ON hbtn_test_db.* TO hbnb_test@localhost;
+-- Grant privileges all privileges on hbnb_test_db to hbnb_test
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost;
 
--- Grant privileges all privileges on performance_schema to hbtn_test
+-- Grant privileges all privileges on performance_schema to hbnb_test
 GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
