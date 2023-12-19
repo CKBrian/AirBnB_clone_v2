787bb66b6fc95d1f5405f9672f40dfb06432f685
Brian <chumobrian@gmail.com>
2023-12-19 18:02:21 +0300

    feat(Filestorage): Add delete method for removing models from the Filstorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,8 +8,12 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls:
+            objs = {key: value for key, value in type(self).__objects.items()
+                    if isinstance(value, cls)}
+            return objs
         return FileStorage.__objects
 
     def new(self, obj):
@@ -45,6 +49,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """deletes an obj from _objects """
+        for key, value in type(self).__objects.items():
+            if obj == value:
+                del type(self).__objects[key]
+                return
