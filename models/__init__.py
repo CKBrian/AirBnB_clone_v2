be7a42a64dd25ee4eb257971c13c085982a35c41
Brian <chumobrian@gmail.com>
2023-12-22 12:53:57 +0300

    feat: Add a conditional depending of the value of the environment variable HBNB_TYPE_STORAGE used to switch between Filestorage and DBstorage in __init__.py script

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
+from os import getenv
 
 
-storage = FileStorage()
-storage.reload()
+# load environment variable
+db_type = getenv("HBNB_TYPE_STORAGE")
+
+# Switch between FileStorage and DBStorage engines
+if db_type == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
+else:
+    storage = FileStorage()
+    storage.reload()
