f9153f87ff8306bbca4a3e7774a4786aa3e10cff
Brian <chumobrian@gmail.com>
2023-12-22 13:01:28 +0300

    feat: Enhance the City class to provide functionality for creating cities table to store cities data in a database

--- models/city.py
+++ models/city.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
+if TYPE_CHECKING:
+    from models.state import State
+else:
+    City = "City"
+
+
+class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    name = Column(String(128), nullable=False)
