2b335498d009126ace0252c77829588b61fb66ff
Brian <chumobrian@gmail.com>
2023-12-22 13:00:46 +0300

    feat: Enhance the State class to provide functionality for creating states table to store states data in a database

--- models/state.py
+++ models/state.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import db_type
 
 
-class State(BaseModel):
+if TYPE_CHECKING:
+    from models.city import City
+else:
+    City = "City"
+
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    if db_type != "db":
+        @property
+        def cities(self):
+            """Gets the states id"""
+            from models.engine import FileStorage
+            curr_objs = FileStorage.all()
+            city_list = [value for key, value in curr_objs
+                         if self.id in key and "City" in key]
+            return city_list
