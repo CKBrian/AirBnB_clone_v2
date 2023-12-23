36f129866ca290113115ed2e1a9d4d9dcfd1a3eb
Brian <chumobrian@gmail.com>
2023-12-23 18:09:30 +0300

    feat(database): Update Review class with columns to store customer reviews in a database

--- models/review.py
+++ models/review.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class Review(BaseModel):
+if TYPE_CHECKING:
+    from models.place import Place
+else:
+    Place = "Place"
+
+
+if TYPE_CHECKING:
+    from models.user import User
+else:
+    User = "User"
+
+
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    text = Column(String(1024), nullable=False)
