6633841bd427840ca82ae472d6005f9f49d865ac
Brian <chumobrian@gmail.com>
2023-12-24 11:16:35 +0300

    feat: Enhance Amenity class to create database table amenities using orm and establish a many to many relationship with Place class

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from __future__ import annotations
+from typing import TYPE_CHECKING
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+if TYPE_CHECKING:
+    from models.place import Place
+else:
+    Place = "Place"
+
+
+class Amenity(BaseModel, Base):
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   back_populates="amenities")
