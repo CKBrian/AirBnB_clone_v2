f075954cd7716ed05d6fe11a8fd1305ac34f7482
Brian <chumobrian@gmail.com>
2024-01-10 19:49:21 +0300

    feat: Add a script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,21 +22,21 @@ sudo mkdir -p /data/web_static/releases/test/
 content="
 <html><body><h1>This is a HBNB releases test page</h1><body/></html>
 "
-echo "$content" > /data/web_static/releases/test/index.html
+echo "$content" | sudo tee /data/web_static/releases/test/index.html
 
 # Creates a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
 if [ -L "/data/web_static/current" ];then
     rm /data/web_static/current
 fi
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
-chown ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 #Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static). Don’t forget to restart Nginx after updating the configuration:
 #Use alias inside your Nginx configuration
 content='\n\tlocation \/hbnb_static {\n\t\talias data/web_static/current/;\n\t\ttry_files $uri $uri\/ =404;\n\t}'
-sed -i "/server_name _;/a\\$content" /etc/nginx/sites-available/default
+sudo sed -i "/server_name _;/a\\$content" /etc/nginx/sites-available/default
 
 # reload nginx
 sudo nginx -s reload
