a337918f1fb6040cdd700c90a19ae2a204c0620f
Brian <chumobrian@gmail.com>
2024-01-11 09:32:50 +0300

    feat: Add a script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 # Install Nginx if it not already installed
 
-sudo apt update && sudo apt install nginx
+sudo apt update -y
+sudo apt install nginx -y
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-content="<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>"
+content="<html><head></head><body>Holberton School</body></html>"
 echo "$content" > /data/web_static/releases/test/index.html
-sudo rm -f /data/web_static/current
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+rm -f /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 content=$"\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "/server_name _;/a\\$content" /etc/nginx/sites-available/default
