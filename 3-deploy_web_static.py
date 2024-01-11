b62218c8c7e9ab357e0daef10dfe36f9335a39f3
Brian <chumobrian@gmail.com>
2024-01-11 16:02:16 +0300

    feat: Add a script that creates and distributes an archive to your web servers, using the function deploy:

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a module that creates and distributes an archive to your web
+servers, using the function deploy"""
+
+
+do_pack = __import__('1-pack_web_static').do_pack
+do_deploy = __import__('2-do_deploy_web_static').do_deploy
+
+
+def deploy():
+    """creates and distributes an archive to your web
+    servers, using the function deploy:"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
