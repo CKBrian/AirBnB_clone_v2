a4efdb9899aac2cab1f2d1d29ba27973c95319b9
Brian <chumobrian@gmail.com>
2024-01-11 09:43:54 +0300

    feat: Add a script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,34 @@
 # Install Nginx if it not already installed
 
 sudo apt update -y
+sudo apt upgrade -y
 sudo apt install nginx -y
+
+# Creates the folder /data/web_static/shared/ if it doesn’t already exist
 sudo mkdir -p /data/web_static/shared/
+
+# Creates the folder /data/web_static/releases/test/ if it doesn’t already exist
 sudo mkdir -p /data/web_static/releases/test/
-content="<html><head></head><body>Holberton School</body></html>"
+
+# Creates a fake HTML file /data/web_static/releases/test/index.html
+content="<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>"
 echo "$content" > /data/web_static/releases/test/index.html
-rm -f /data/web_static/current
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Creates a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
+sudo rm /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# Give ownership of the /data/ folder to the ubuntu user AND group .
 sudo chown -hR ubuntu:ubuntu /data/
+
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 content=$"\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "/server_name _;/a\\$content" /etc/nginx/sites-available/default
+# reload nginx
 sudo service nginx restart
