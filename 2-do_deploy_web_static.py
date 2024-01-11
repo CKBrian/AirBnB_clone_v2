9444a982576389f18771ce3a3ddadbd0bd6e244d
Brian <chumobrian@gmail.com>
2024-01-11 17:23:18 +0300

    feat: Add a script that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,12 +24,12 @@ def do_deploy(archive_path):
         run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
             .format(ar_dir, ar_dir))
         run("rm /tmp/{}.tgz".format(ar_dir))
-        run("rm /data/web_static/current")
+        run("rm -rf /data/web_static/current")
         path = f"/data/web_static/releases/{ar_dir}/web_static"
-        run(f"cp -r {path}/* /data/web_static/releases/{ar_dir}")
+        run(f"mv {path}/* /data/web_static/releases/{ar_dir}/")
         run(f"rm -rf {path}")
         Dir = "/data/web_static"
         run(f"ln -sf {Dir}/releases/{ar_dir}/ {Dir}/current")
         return True
-    except:
+    except Exception as e:
         return False
