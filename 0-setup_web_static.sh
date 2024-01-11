d21ca004a84e70f325a7b1cfb8efbfa198edb00c
Brian <chumobrian@gmail.com>
2024-01-11 09:10:17 +0300

    feat: Add a script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,14 +2,8 @@
 # Install Nginx if it not already installed
 
 sudo apt update && sudo apt install nginx
-
-# Creates the folder /data/web_static/shared/ if it doesn’t already exist
 sudo mkdir -p /data/web_static/shared/
-
-# Creates the folder /data/web_static/releases/test/ if it doesn’t already exist
 sudo mkdir -p /data/web_static/releases/test/
-
-# Creates a fake HTML file /data/web_static/releases/test/index.html
 content="<html>
   <head>
   </head>
@@ -18,15 +12,8 @@ content="<html>
   </body>
 </html>"
 echo "$content" | sudo tee /data/web_static/releases/test/index.html
-
-# Creates a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ folder to the ubuntu user AND group .
 sudo chown -hR ubuntu:ubuntu /data/
-
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
 content=$"\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "/server_name _;/a\\$content" /etc/nginx/sites-available/default
-# reload nginx
 sudo service nginx restart
