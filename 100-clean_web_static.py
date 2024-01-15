fcac2c9ae7578274dbec0f6f18aa5e14bd81bb07
Brian <chumobrian@gmail.com>
2024-01-15 05:52:27 +0300

    feat: Adda script that deletes out-of-date archives, using the function do_clean:

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -2,7 +2,8 @@
 """defines a module that  that deletes out-of-date archives,
     using the function do_clean:"""
 
-from fabric.api import local, sudo, cd
+from fabric.api import *
+from fabric.state import *
 import os
 
 env.hosts = ['100.26.173.252', '54.160.114.174']
@@ -12,24 +13,22 @@ env.key_filename = "my_ssh_private_key"
 
 def do_clean(number=0):
     """Deletes old archives"""
-    local_versions = local('ls -t ~/AirBnB_Clone_V2/versions/',
-                           capture=True).split()
-    remote_versions = sudo('ls -t /data/web_static/releases/',
-                           quiet=True).split()
-
-    number = int(number)
-    num_to_keep = 1 if number <= 0 else number
-
-    if len(local_versions) > num_to_keep:
-        local_to_delete = local_versions[num_to_keep:]
-        for archive in local_to_delete:
-            local('rm -f ~/AirBnB_Clone_V2/versions/{}'.format(archive))
-
+    local('ls -t ~/AirBnB_Clone_V2/versions/').split()
     with cd("/data/web_static/releases"):
-        remote_versions = sudo("ls -t .").split()
-
-    if len(remote_versions) > num_to_keep:
-        remote_to_delete = remote_versions[num_to_keep:]
-        for archive in remote_to_delete:
-            sudo('rm -rf {}/{}'.format("/data/web_static/releases",
-                                       archive.strip(".tgz")))
+        magic = sudo("ls -t .").split()
+    paths = "/data/web_static/releases"
+    number = int(number)
+    if number == 0:
+        num = 1
+    else:
+        num = number
+    if len(magic) > 0:
+        if len(target) == number or len(target) == 0:
+            pass
+        else:
+            cl = target[num:]
+            for i in range(len(cl)):
+                local('rm -f ~/AirBnB_Clone_V2/versions/{}'.format(target[-1]))
+        rem = magic[num:]
+        for j in range(len(rem)):
+            sudo('rm -rf {}/{}'.format(paths, rem[-1].strip(".tgz")))
